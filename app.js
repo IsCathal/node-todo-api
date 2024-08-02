9f6a8dceee3ff31e4bf9ebf5c7d5125029001be0
IsCathal <mccabemcathal@gmail.com>
2024-08-02 11:33:37 +0100

    mongoose fun

--- app.js
+++ app.js
@@ -2,33 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const router = express.Router();
+
+const Todo = require('./todo');
  
 router.get('/', (req, res) => res.send('Hello World!'));
  
 router.route('/todos')
   .get((req, res) => {
-      res.json([
-         {
-           "_id": 1,
-           "text": "Item 1",
-           "done": false
-         },
-         {
-           "_id": 2,
-           "text": "Item 2",
-           "done": false
-         },
-         {
-           "_id": 3,
-           "text": "Item 3",
-           "done": true
-         },
-         {
-           "_id": 4,
-           "text": "Item 4",
-           "done": false
-         },
-      ]);
+    Todo.all(function(data) {
+      res.json(data);
+    })
   })
  
 app.use('/', router);
---
+++ todo.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {  
+  console.log('Connected to MongoDB');
+}   );  
+
+const todoSchema = new mongoose.Schema({  
+  text: String,  
+  done: Boolean
+});
+
+todoSchema.statics.all = function(callback) {
+  return Todo.find([], function(err, todos) { 
+        callback({todos: todos});
+      });
+    };
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
+
